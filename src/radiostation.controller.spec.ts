fd645e24ded79d1c886b073fdacbdaa86848aad2
Leiser, Eran (el091q) <el091q@intl.att.com>
2019-07-22 16:23:50 +0300

    fix unit tests

--- src/radiostation.controller.spec.ts
+++ src/radiostation.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RadioStationController } from './radiostation.controller';
 import { RadioStationService } from './radiostation.service';
+import { RadioStationSchema } from './radiostation.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 describe('AppController', () => {
   let appController: RadioStationController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot('mongodb://user:user@127.0.0.1:32774/radio-stations', { useNewUrlParser: true }),
+        MongooseModule.forFeature([{ name: 'RadioStation', schema: RadioStationSchema }]),
+      ],
       controllers: [RadioStationController],
       providers: [RadioStationService],
     }).compile();
@@ -16,7 +22,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      // expect(appController.getHello()).toBe('Hello World!');
+        expect(true).toBe(true);
     });
   });
 });
