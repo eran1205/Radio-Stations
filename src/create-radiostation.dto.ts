6a5a0ba011f4732271e260fb762613b00cf99b67
Leiser, Eran (el091q) <el091q@intl.att.com>
2019-07-22 16:07:36 +0300

    Add travis file and script from git-push

--- src/create-radiostation.dto.ts
+++ src/create-radiostation.dto.ts
@@ -2,5 +2,6 @@ export class CreateRadioStationDTO {
     readonly title: string;
     readonly description: string;
     readonly logo: string;
+    // tslint:disable-next-line:variable-name
     readonly created_at: Date;
 }
